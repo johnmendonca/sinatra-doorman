d6e647ff098c2a6520b020d88c377d2227a31f5c
John Mendonca <rangerfrex@hotmail.com>
2010-02-06 13:40:38 -0800

    return all extensions back to one file

--- lib/doorman/core.rb
+++ lib/doorman/core.rb
@@ -117,5 +117,138 @@ module Sinatra
         end
       end
     end
+
+    #
+    # Remember Me
+    #
+
+    COOKIE_KEY = "sinatra.doorman.remember"
+
+    module RememberMe
+      class RememberMeStrategy < Warden::Strategies::Base
+        def valid?
+          !!env['rack.cookies'][COOKIE_KEY]
+        end
+
+        def authenticate!
+          token = env['rack.cookies'][COOKIE_KEY]
+          return unless token
+          user = User.first(:remember_token => token)
+          env['rack.cookies'].delete(COOKIE_KEY) and return if user.nil?
+          success!(user)
+        end
+      end
+
+      def self.registered(app)
+        app.use Rack::Cookies
+
+        Warden::Strategies.add(:remember_me, RememberMeStrategy)
+
+        app.before do
+          env['warden'].authenticate(:remember_me)
+        end
+
+        Warden::Manager.after_authentication do |user, auth, opts|
+          if auth.winning_strategy.is_a?(RememberMeStrategy) ||
+            (auth.winning_strategy.is_a?(Core::PasswordStrategy) &&
+               auth.params['user']['remember_me'])
+            user.remember_me!  # new token
+            auth.env['rack.cookies'][COOKIE_KEY] = { 
+              :value => user.remember_token, 
+              :expires => Time.now + 7 * 24 * 3600, 
+              :path => '/' }
+          end
+        end
+
+        Warden::Manager.before_logout do |user, auth, opts|
+          user.forget_me! if user
+          auth.env['rack.cookies'].delete(COOKIE_KEY)
+        end
+      end
+    end
+
+    #
+    # Forgot Password
+    #
+
+    module ForgotPassword
+      def self.registered(app)
+        Warden::Manager.after_authentication do |user, auth, opts|
+          # If the user requested a new password,
+          # but then remembers and logs in,
+          # then invalidate password reset token
+          if auth.winning_strategy.is_a?(Core::PasswordStrategy)
+            user.remembered_password!
+          end
+        end
+
+        app.get '/forgot/?' do
+          redirect '/home' if authenticated?
+          haml :forgot
+        end
+
+        app.post '/forgot' do
+          redirect '/home' if authenticated?
+          redirect '/' unless params['user']
+
+          user = User.first_by_login(params['user']['login'])
+
+          if user.nil?
+            flash[:error] = Messages[:forgot_no_user]
+            redirect '/forgot'
+          end
+
+          user.forgot_password!
+          Pony.mail(
+            :to => user.email, 
+            :from => "no-reply@#{env['SERVER_NAME']}", 
+            :body => token_link('reset', user))
+          flash[:notice] = Messages[:forgot_success]
+          redirect '/login'
+        end
+
+        app.get '/reset/:token/?' do
+          redirect '/home' if authenticated?
+
+          if params[:token].nil? || params[:token].empty?
+            flash[:error] = Messages[:reset_no_token]
+            redirect '/'
+          end
+
+          user = User.first(:confirm_token => params[:token])
+          if user.nil?
+            flash[:error] = Messages[:reset_no_user]
+            redirect '/login'
+          end
+
+          haml :reset, :locals => { :confirm_token => user.confirm_token }
+        end
+
+        app.post '/reset' do
+          redirect '/home' if authenticated?
+          redirect '/' unless params['user']
+          
+          user = User.first(:confirm_token => params[:user][:confirm_token])
+          if user.nil?
+            flash[:error] = Messages[:reset_no_user]
+            redirect '/login'
+          end
+
+          success = user.reset_password!(
+            params['user']['password'], 
+            params['user']['password_confirmation'])
+
+          unless success
+            flash[:error] = Messages[:reset_unmatched_passwords]
+            redirect "/reset/#{user.confirm_token}"
+          end
+
+          user.confirm_email!
+          env['warden'].set_user(user)
+          flash[:notice] = Messages[:reset_success]
+          redirect '/home'
+        end
+      end
+    end
   end
 end
